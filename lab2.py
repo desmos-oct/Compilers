55b8a2b44bd0d134834f3ee76de7658938684390
desmos-oct <desmosoct@gmail.com>
2022-11-06 21:20:18 +0800

    complete lab2 without debugging

--- lab2.py
+++ lab2.py
@@ -1,10 +1,19 @@
 import re
 
+i = 0
+
+lines = []
+
+with open("rst.txt","r") as f:
+    lines = f.readlines()
+    for line in lines:
+        if line.split()[0]=="wrong":
+           lines.remove(line) 
+
 def S():
-    V()
-    if matches('equal'):
-        E()
-    return True
+    if V() and matches('equal') and E():
+        return True
+    return False
 
 def V():
     if matches('identifier'):
@@ -12,93 +21,106 @@ def V():
     return False
 
 def E():
-    T()
-    E2()
+    if T() and E2():
+        return True
 
 def E2():
-    A()
-    T()
-    E2()
+    preI = i
+    if A() and T() and E():
+        return True
+    else:
+        i = preI
+        return True
 
 def T():
-    F()
-    T2()
+    if F() and T2():
+        return True
+    else:
+        return False
 
 def T2():
-    M()
-    F()
-    T2()
+    preI = i
+    if M() and F() and T2():
+        return True
+    else:
+        i = preI
+        return True
 
 def F():
+    preI = i
     if matches('identifier'):
         return True
-    elif matches('left_paren'):
-        E()
-        if matches('right_paren'):
-            return True
+    i = preI
+    if matches('left_paren') and E() and matches("right_paren"):
+        return True
     return False
 
 def A():
+    preI = i
     if matches('plus'):
         return True
-    elif matches('minus'):
+    i = preI
+    if matches('minus'):
         return True
     return False
 
 def M():
+    preI = i
     if matches('times'):
         return True
-    elif matches('divide'):
+    i = preI
+    if matches('divide'):
         return True
     return False
 
 def matches(str):
-    with open("rst.txt","r") as f:
-        if str == 'identifier':
-            if f.readline()[1:3]=="1 ":
-                return True
-            else:
-                return False
-        elif str == 'number':
-            if f.readline()[1:3]=="3 ":
-                return True
-            else:
-                return False
-        elif str == 'equal':
-            if f.readline()[1:3]=="10":
-                return True
-            else:
-                return False
-        elif str == 'left_paren':
-            if f.readline()=="(11 ()":
-                return True
-            else:
-                return False
-        elif str == 'right_paren':
-            if f.readline()=="(11 ))":
-                return True
-            else:
-                return False
-        elif str == 'plus':
-            if f.readline()=="(11 +)":
-                return True
-            else:
-                return False
-        elif str == 'minus':
-            if f.readline()=="(11 -)":
-                return True
-            else:
-                return False
-        elif str == 'times':
-            if f.readline()=="(11 *)":
-                return True
-            else:
-                return False
-        elif str == 'divide':
-            if f.readline()=="(11 /)":
-                return True
-            else:
-                return False
+    if str == 'identifier':
+        if lines[i][1:3]=="1 ":
+            return True
+        else:
+            return False
+    elif str == 'number':
+        if lines[i][1:3]=="3 ":
+            return True
+        else:
+            return False
+    elif str == 'equal':
+        if lines[i][1:3]=="10":
+            return True
+        else:
+            return False
+    elif str == 'left_paren':
+        if lines[i]=="(11 ()":
+            return True
+        else:
+            return False
+    elif str == 'right_paren':
+        if lines[i]=="(11 ))":
+            return True
+        else:
+            return False
+    elif str == 'plus':
+        if lines[i]=="(11 +)":
+            return True
+        else:
+            return False
+    elif str == 'minus':
+        if lines[i]=="(11 -)":
+            return True
+        else:
+            return False
+    elif str == 'times':
+        if lines[i]=="(11 *)":
+            return True
+        else:
+            return False
+    elif str == 'divide':
+        if lines[i]=="(11 /)":
+            return True
+        else:
+            return False
+    i += 1
+
 
 def main():
     S()
